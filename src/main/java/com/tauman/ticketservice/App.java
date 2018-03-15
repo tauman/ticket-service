05972299e3662fadca76d413c1550849093327ad
Steven Reich <tauman@yahoo.comn>
2018-03-15 18:06:34 -0400

    Add App stub

---
+++ src/main/java/com/tauman/ticketservice/App.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tauman.ticketservice;
+
+/**
+ *
+ * @author tauman
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("\n\n************************* ticket-service *************************");        
+        System.out.println("\nThis project does not have an executable.\n");
+        System.out.println("To run the tests, run one of the following:\n");
+        System.out.println("    gradle test             (This will show output only the first\n");
+        System.out.println("                             time or when there are code changes)\n");
+        System.out.println("    gradle cleanTest test   (This will always show output\n");
+        System.out.println("You can add the --info flag to either of those to see logging output)");
+        System.out.println("\n******************************************************************\n\n");        
+    }
+}
